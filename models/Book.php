4e27a1d062a0151b2c33bcfa7aa2b2722103c0b8
Hani <haniabukhait@gmail.com>
2019-09-24 05:11:18 +0300

    added comments to clarify

--- models/Book.php
+++ models/Book.php
@@ -13,6 +13,9 @@ class Book extends BaseModel
     ];
 
 
+    // we are receiving title, author_id, tags, image
+    // our table "books" should receive lastId inserted id, title, author_id, book_image
+
     protected $table = "books";
 
     public function insertBook(array $data)
@@ -20,15 +23,15 @@ class Book extends BaseModel
         //sql
         $tagModel = new BookTags();
         $lastId = "select max(id) id from " . $this->table . "";
-        $imageName = $this->uploadPhoto($data['image'])["name"];
-        $tags = $data['tags'];
-        unset($data['image']);
-        unset($data['tags']);
-        $data['book_image'] = $imageName;
-        $this->insert($data);
-        $insertedId = $this->fetchRaw($lastId);
-        $bookId =  $insertedId[0]["id"];
-        foreach ($tags as $tag) {
+        $imageName = $this->uploadPhoto($data['image'])["name"]; // go inside image array and get the name 'image' => 'name.jpg'
+        $tags = $data['tags']; // go inside tags table get the ids selected
+        unset($data['image']); // remove 'image' only from 'image'=>'name'
+        unset($data['tags']); // remove 'tags' only from 'image'=>'name'
+        $data['book_image'] = $imageName; // add book_image to get book_image => name.jpg
+        $this->insert($data); // modify inserted data
+        $insertedId = $this->fetchRaw($lastId); //fetch last inserted id raw
+        $bookId =  $insertedId[0]["id"]; //get last id number inserted
+        foreach ($tags as $tag) { //go inside tags and get the tag foreach raw
             $tagModel->insert(array(
                 "tag_id" => $tag,
                 "book_id" => $bookId
@@ -84,9 +87,9 @@ class Book extends BaseModel
                 throw new Error("Only JPG, JPEG, PNG, GIF files are allowed.");
             }
             // make sure file does not exist
-            if (file_exists($target_file)) {
-                throw new Error("Image already exists. Try to change file name.");
-            }
+            // if (file_exists($target_file)) {
+            //     throw new Error("Image already exists. Try to change file name.");
+            // }
             // make sure submitted file is not too large, can't be larger than 1 MB
             if ($image['size'] > (99999999999999999999)) {
                 throw new Error("Image must be less than 1 MB in size.");
