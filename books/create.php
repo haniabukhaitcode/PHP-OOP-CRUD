d10a70b3ab591d943e53201ae330080f3983455f
haniabukhaitcode <haniabukhait@gmail.com>
2019-09-30 17:17:52 +0300

    Main page edit working, need to fix insert

--- books/create.php
+++ books/create.php
@@ -18,54 +18,51 @@ if ($_POST) {
 <div class="container mt-4">
     <div class="row">
         <div class="col-lg-12">
-            <div class="container mt-4">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="jumbotron">
-                            <h4 class="mb-4">Add Books</h4>
+            <div class="jumbotron">
+                <h4 class="mb-4">Add Books</h4>
 
-                            <form action="" method="post" enctype="multipart/form-data">
-                                <div class="form-group">
-                                    <label for="title">Title</label>
-                                    <input type="text" name="title" class="form-control" placeholder="Enter book name">
-                                </div>
+                <form action="" method="post" enctype="multipart/form-data">
+                    <div class="form-group">
+                        <label for="title">Title</label>
+                        <input type="text" name="title" class="form-control" placeholder="Enter book name">
+                    </div>
 
-                                <div class="form-group">
-                                    <label>Author</label>
-                                    <div class="mb-3">
-                                        <select class='form-control' name="author_id">
-                                            <?php
-                                            $author = new Author();
-                                            foreach ($author->fetchAll() as $row) : ?>
-                                                <option value="<?= $row->id; ?>"><?= $row->author; ?></a></option>
-                                            <?php endforeach; ?>
-                                        </select>
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <label>Tag</label>
-                                    <select class='form-control' name="tags[]" multiple="multiple">
-                                        <?php
-                                        $tag = new Tag();
-                                        foreach ($tag->fetchAll() as $row) : ?>
-                                            <option value="<?= $row->id; ?>"><?= $row->tag; ?></a></option>
-                                        <?php endforeach; ?>
-                                    </select>
-                                </div>
-                                <div class="form-group">
-                                    <div class="mb-3">
-                                        <input type="file" name="book_image" id="book_image">
-                                    </div>
-                                </div>
-
-                                <input type="submit" name="submit" class="btn btn-primary" />
-                            </form>
-
+                    <div class="form-group">
+                        <label>Author</label>
+                        <div class="mb-3">
+                            <select class='form-control' name="author_id">
+                                <?php
+                                $author = new Author();
+                                foreach ($author->fetchAll() as $row) : ?>
+                                    <option value="<?= $row->id; ?>"><?= $row->author; ?></a></option>
+                                <?php endforeach; ?>
+                            </select>
                         </div>
                     </div>
-                </div>
+                    <div class="form-group">
+                        <label>Tag</label>
+                        <select class='form-control' name="tags[]" multiple="multiple">
+                            <?php
+                            $tag = new Tag();
+                            foreach ($tag->fetchAll() as $row) : ?>
+                                <option value="<?= $row->id; ?>"><?= $row->tag; ?></a></option>
+                            <?php endforeach; ?>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <div class="mb-3">
+                            <input type="file" name="book_image" id="book_image">
+                        </div>
+                    </div>
+
+                    <input type="submit" name="submit" class="btn btn-primary" />
+                </form>
+
             </div>
+        </div>
+    </div>
+</div>
 
-            </body>
+</body>
 
-            </html>
+</html>
--- books/edit.php
+++ books/edit.php
@@ -1,16 +1,14 @@
+<?php require_once('../navbar.html'); ?>
 <?php
 $id = isset($_GET['id']) ? $_GET['id'] : die('ERROR: missing ID.');
+
 require_once("../db/BaseModel.php");
 require '../models/Book.php';
-require '../models/Author.php';
-require '../models/Tag.php';
 
-$book = new Book;
-$author = new Author;
-$tag = new Tag;
-// $book->readOne($id);
 
 if ($_POST) {
+
+    $book = new Book;
     $book->updateBook(
 
         $_GET['id'],
@@ -18,7 +16,7 @@ if ($_POST) {
         [
             "title" => $_POST["title"],
             "author_id" => $_POST["author_id"],
-            "tags" => $_POST["tag_id"],
+            "tags" => $_POST["tags"],
             "image" => $_FILES["book_image"]
         ]
 
@@ -27,77 +25,66 @@ if ($_POST) {
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <title>Create Student</title>
-</head>
-
-<body>
-    <div class="container p-4">
-        <div class="card">
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-12">
-                        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"] . "?id={$id}"); ?>" method="post" enctype="multipart/form-data">
-                            <div>
-                                <label>Title</label>
-                                <input type='text' name='title' class='form-control' /></label>
-                            </div>
-                            <div class="mt-3">
-                                <label>Author</label>
-                                <select class=' form-control' name='author_id'>
-                                    <?php
-                                    $author = new Author();
-                                    foreach ($author->fetchRaw() as $row) :
-                                        if ($book->author_id == $row['id']) :
-                                            ?>
-                                            <option selected value="<?= $row['id'] ?>"><?= $row['author'] ?></option>
-                                        <?php else : ?>
-                                            <option value="<?= $row['id'] ?>"><?= $row['author'] ?></option>
-                                    <?php endif;
-                                    endforeach; ?>
-                                </select>
-                            </div>
-                            <div class="mt-3">
-                                <label>Tag</label>
-                                <select class=' form-control' name='tag_id[]' multiple='multiple'>
-                                    <?php
-                                    $tag = new Tag();
-                                    foreach ($tag->fetchRaw() as $row) : ?>
-
-                                        <option value=<?= $row['id'] ?>><?= $row['tag'] ?></option>
 
 
-                                    <?php endforeach; ?>
 
-
-                                </select>
-                                <div>
-                                    <div class="mt-3">
-                                        <label>Image</label>
-                                        <input type="file" name="book_image" id="book_image">
-                                        <?php
-                                        if (!empty($book->book_image)) : ?>
-                                            <td>
-                                                <img src="/books/uploads/' . $book->book_image . '" alt="" />
-                                            </td>
-                                        <?php endif; ?>
-                                    </div>
-                                    <div class="mt-3">
-                                        <button type="submit" class="btn btn-primary">Update</button>
-                                    </div>
-                        </form>
+<div class="container mt-4">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="jumbotron">
+                <h4 class="mb-4">Add Books</h4>
+                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"] . "?id={$id}"); ?>" method="post" enctype="multipart/form-data">
+                    <div>
+                        <label>Title</label>
+                        <input type='text' name='title' class='form-control' /></label>
                     </div>
-                </div>
+                    <div class="mt-3">
+                        <label>Author</label>
+                        <select class=' form-control' name='author_id'>
+                            <?php
+                            require '../models/Author.php';
+                            $author = new Author();
+                            foreach ($author->fetchRaw() as $row) :
+                                if ($book->author_id == $row['id']) :
+                                    ?>
+                                    <option selected value="<?= $row['id'] ?>"><?= $row['author'] ?></option>
+                                <?php else : ?>
+                                    <option value="<?= $row['id'] ?>"><?= $row['author'] ?></option>
+                            <?php endif;
+                            endforeach; ?>
+                        </select>
+                    </div>
+                    <div class="mt-3">
+                        <label>Tag</label>
+                        <select class=' form-control' name='tags[]' multiple='multiple'>
+                            <?php
+                            require '../models/Tag.php';
+                            $tag = new Tag();
+                            foreach ($tag->fetchRaw() as $row) : ?>
+
+                                <option value=<?= $row['id'] ?>><?= $row['tag'] ?></option>
+
+
+                            <?php endforeach; ?>
+
+
+                        </select>
+                        <div>
+                            <div class="mt-3">
+                                <label>Image</label>
+                                <input type="file" name="book_image" id="book_image">
+                                <?php
+                                if (!empty($book->book_image)) : ?>
+                                    <td>
+                                        <img src="/books/uploads/' . $book->book_image . '" alt="" />
+                                    </td>
+                                <?php endif; ?>
+                            </div>
+                            <div class="mt-3">
+                                <button type="submit" class="btn btn-primary">Update</button>
+                            </div>
+                </form>
             </div>
         </div>
     </div>
-</body>
-
-</html>
+</div>
