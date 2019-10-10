441a019cee4cbcacaa83c957418c04f4d08602d8
Hani <haniabukhait@gmail.com>
2019-10-10 06:35:01 +0300

    ajax

--- authors/delete.php
+++ authors/delete.php
@@ -1,17 +1,8 @@
 <?php
 require_once "../models/Author.php";
-require_once '../navbar.html';
-require_once '../header.html';
 
+if (isset($_POST['author'])) {
 
-if (isset($_GET['del'])) {
-    $id = $_GET['del'];
     $author = new Author;
-    $author->delete(array('id' => $id));
+    $author->author = $_POST['author'];
 }
-?>
-
-
-<h1>Are you sure you want to delete <a class="btn btn-sm btn-success col-1" href="index.php">No</a> &nbsp; <a class="btn btn-sm btn-danger col-1" href="delete.php?del=<?= $_GET['id'] ?>">yes</a></h1>
-
-<?php require_once '../footer.html'; ?>
---
+++ authors/handler.php
@@ -0,0 +1,31 @@
+<?php
+
+
+if (isset($_POST['handler'])) {
+    ?>
+    <table class="table table-dark">
+        <thead>
+            <th>ID</th>
+            <th>Author</th>
+            <th>Actions</th>
+        </thead>
+        <tbody>
+            <?php
+                require_once '../models/Author.php';
+                $author = new Author;
+                foreach ($author->fetchAll() as $row) : print_r($author) ?>
+                <tr>
+                    <th scope="row"><?= $row->id; ?></th>
+                    <td><span id="author<?= $row->id; ?>"><?= $row->author; ?></span></td>
+                    <td>
+                        <a style="cursor:pointer;" class="btn btn-sm btn-primary" data-id="<?= $row->id; ?>"> Edit</a>&nbsp;
+                        <a style="cursor:pointer;" class="btn btn-sm btn-danger" data-id="<?= $row->id; ?>"> Delete</a>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+
+        </tbody>
+    </table>
+<?php
+}
+?>
